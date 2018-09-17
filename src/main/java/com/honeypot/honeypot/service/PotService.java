8954399bfe00e5717fc5a2c05dbae69ddbe55416
Tahir <171048134@q.com>
2018-09-17 16:31:17 +0800

    9.17主机蜜罐管理页面后台
    主机蜜罐管理页面后台

--- src/main/java/com/honeypot/honeypot/service/PotService.java
+++ src/main/java/com/honeypot/honeypot/service/PotService.java
@@ -8,9 +8,10 @@ import java.util.Map;
 public interface PotService {
     /**
      * 应该根据类型获取蜜罐信息，暂时写死
+     * getHostPotByType 是主机蜜罐管理页面的方法
      * @return
      */
     List<Pot> getPotByType(String type);
-
+    List<Pot> getHostPotByType(String type);
     List<Pot> getPotByIp(String ip);
 }
